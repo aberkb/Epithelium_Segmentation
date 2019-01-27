f9618d3ce4c84fbe5579d7914fb0eb838f3eb4b9
Ahmet Berk Bektaş <ahmetberkbektas@gmail.com>
2019-01-27 23:35:31 +0300

    Adjusted params

--- patch_generation.m
+++ patch_generation.m
@@ -1,27 +1,23 @@
 outdir='./patches/';
 mkdir(strrep(outdir,'..','.'));
-patchSz = [200 200];
+patchSize = [200 200];
 
-imSz = [1000 1000];
-xIdxs = [1:patchSz(2):imSz(2) imSz(2)+1];
-yIdxs = [1:patchSz(1):imSz(1) imSz(1)+1];
-patches = cell(length(yIdxs)-1,length(xIdxs)-1);
-imagefiles = dir('*.tif');      
+imgSize = [1000 1000];
+xIndex = [1:patchSize(2):imgSize(2) imgSize(2)+1];
+yIndex = [1:patchSize(1):imgSize(1) imgSize(1)+1];
+patches = cell(length(yIndex)-1,length(xIndex)-1);
+imagefiles = dir('*.tif');
 nfiles = length(imagefiles);    % Number of files found
 for ii=1:nfiles
-   currentfilename = imagefiles(ii).name;
-   I = imread(currentfilename);
-   imSz = size(I);
-    for i = 1:length(yIdxs)-1
-        Isub = I(yIdxs(i):yIdxs(i+1)-1,:);
-        for j = 1:length(xIdxs)-1
-            pat = Isub(:,xIdxs(j):xIdxs(j+1)-1);
-            pname=sprintf('%d_e_%d___%s.png',i,j,currentfilename); %generate a filename for it
+   currentFileName = imagefiles(ii).name;
+   I = imread(currentFileName);
+   imgSize = size(I);
+    for i = 1:length(yIndex)-1
+        Isub = I(yIndex(i):yIndex(i+1)-1,:);
+        for j = 1:length(xIndex)-1
+            pat = Isub(:,xIndex(j):xIndex(j+1)-1);
+            pname=sprintf('%d_e_%d___%s.png',i,j,currentfilename); %generate a filename for each img
             imwrite(pat,sprintf('%s/%s',outdir,pname)); %save the image patch to the output directoy
         end
-
     end
 end
-
-
-
